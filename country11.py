4b592e971fad8b7e2fb95a11ab6867166bc4ba7b
litletooth <you@example.com>
2022-01-14 09:44:58 +0900

    new

---
+++ country11.py
@@ -0,0 +1,9 @@
+home=str(input("国はどこですか"))
+if home =="日本":
+    print("hello.japan")
+elif home=="タイ":
+    print("hello.Thailand")
+elif home=="インド":
+    print("hello,india")
+else:
+    print("hello,world")
